17a25054f8522172a190512e017721729e90db88
Erik <nuclear.ace@gmail.com>
2016-12-31 11:52:43 -0500

    handle indirect enum cases

--- Sources/Reflection/Metadata+Enum.swift
+++ Sources/Reflection/Metadata+Enum.swift
@@ -18,20 +18,34 @@ extension Metadata {
 
         public var cases: [EnumCase] {
             let caseNames = nominalTypeDescriptor.fieldNames
-            let payloadCases = Array(zip(caseNames, caseTypes)).map({ EnumCase(name: $0.0, type: $0.1) })
-            let nonPayloadCases = Array(caseNames.dropFirst(payloadCases.count)).map({ EnumCase(name: $0, type: nil) })
+            let payloadCases = Array(zip(caseNames, caseTypes)).map({
+                EnumCase(indirect: $1.indirect, name: $0, type: $1.type)
+            })
+            let nonPayloadCases = Array(caseNames.dropFirst(payloadCases.count)).map({
+                EnumCase(indirect: false, name: $0, type: nil)
+            })
 
             return payloadCases + nonPayloadCases
         }
 
-        public var caseTypes: [Any.Type] {
+        private var caseTypes: [(type: Any.Type, indirect: Bool)] {
             guard numberOfPayloadCases > 0 else { return [] }
             guard let function = nominalTypeDescriptor.fieldTypesAccessor else { return [] }
 
             let typePointers = function(UnsafePointer<Int>(pointer))
             let buffer = UnsafeBufferPointer<UnsafePointer<Int>>(start: typePointers, count: numberOfPayloadCases)
 
-            return buffer.map({ unsafeBitCast($0, to: Any.Type.self) })
+            return buffer.map({pointer in
+                var pointer = pointer
+                let address = Int(bitPattern: pointer)
+                let isIndirect = address & 1 == 1
+
+                if isIndirect {
+                    pointer = UnsafePointer<Int>(bitPattern: address - 1)!
+                }
+
+                return (unsafeBitCast(pointer, to: Any.Type.self), isIndirect)
+            })
         }
 
         public var nominalTypeDescriptorOffsetLocation: Int {
@@ -41,6 +55,7 @@ extension Metadata {
 }
 
 public struct EnumCase {
+    public let indirect: Bool
     public let name: String
     public let type: Any.Type?
 }
